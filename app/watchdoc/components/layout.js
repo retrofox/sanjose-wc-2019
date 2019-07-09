a4b97593d6c901e662eaf1aa3afb1b22f55fe7ca
retrofox <rdsuarez@gmail.com>
2019-07-09 17:46:51 -0300

    improve layout adding `menu` and `footer`

--- app/watchdoc/components/layout.js
+++ app/watchdoc/components/layout.js
@@ -1,1 +1,32 @@
-export default ({children}) => <div>{children}</div>;
+import Head from 'next/head'
+
+export default ({ children, title = 'Watchdog' }) => (
+	<div>
+		<Head>
+			  <title>{title}</title>
+			  <meta charSet='utf-8' />
+			  <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+		</Head>
+	<header>
+	  	<nav>
+			<ul>
+				<li>
+					<a href='/'>🐶 Watchdog</a>
+				</li>
+
+				<li>
+					<a href='/logout'>Login</a>
+				</li>
+
+				<li>
+					<a href='/contact'>Contacto</a>
+				</li>
+			</ul>
+	  	</nav>
+	</header>
+
+	{children}
+
+	<footer>San José WordCamp @2019</footer>
+  </div>
+)
