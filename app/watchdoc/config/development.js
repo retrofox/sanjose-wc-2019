1ca5b2305a83592abaec99edc997eff75acb6a97
retrofox <rdsuarez@gmail.com>
2019-09-05 15:55:28 -0600

    rebase: setting config module

--- app/watchdoc/config/development.js
+++ app/watchdoc/config/development.js
@@ -1,9 +1,12 @@
 const  config =  {
-  APP_HOST: 'http://localhost',
-  APP_PORT: '3000',
+	APP_HOST: 'http://localhost',
+	APP_PORT: '3000',
 
-  API_CLIENT_ID: "66236",
-  API_AUTHORIZE_URL: "https://public-api.wordpress.com/oauth2/authorize",
+	API_CLIENT_ID: '66838',
+	APP_CLIENT_SECRET: 'oz8vST8zK5TLt9mBkbfcO8oVDI6qr9xIwJGEFrna9W0QzmrsJiLOwE3sWJbmezQf',
+	API_AUTHORIZE_URL: 'https://public-api.wordpress.com/oauth2/authorize',
+	API_TOKEN_URL: 'https://public-api.wordpress.com/oauth2/token-info',
+	API_CONNECTION_ARGS: { scope: 'global', response_type: 'token' },
 };
 
 config.API_REDIRECT_URL = `${config.APP_HOST}:${config.APP_PORT}`;
