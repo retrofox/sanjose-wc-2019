bbf8c698c32c05618041127583a456314b45d41b
retrofox <rdsuarez@gmail.com>
2019-09-05 09:12:00 -0600

    improving styles in the whole app

--- app/watchdoc/components/layout-footer.js
+++ app/watchdoc/components/layout-footer.js
@@ -1,3 +1,12 @@
-export default () => <div>
-    San José WordCamp @2019
+export default () => <div className="footer" style={{
+	width: '100%',
+	textAlign: 'center',
+	position: 'absolute',
+	bottom: '0',
+	height: '40px',
+	lineHeight: '40px',
+}}>
+	<a href="https://2019.sanjose.wordcamp.org">
+		San José WordCamp @2019
+	</a>
 </div>
--- app/watchdoc/components/layout-menu.js
+++ app/watchdoc/components/layout-menu.js
@@ -4,29 +4,42 @@ export default () => <nav>
 	<style jsx>{`
 		nav {
 			width: 100%;
-			background-color: #f0f0f0;
-			padding: 5px 0;
+			background-color: #222;
+			border-bottom: 1px solid #040;
+			position: absolute;
+			z-index: 10;
+			top: 0;
+			left: 0;
+			height: 60px;
+			padding-top: 14px;
 		}
  
 		nav ul,
 		nav ul li {
 			list-style: none;
+			margin: 0;
+			padding: 0;
+		}
+		
+		nav ul {
+			margin-left: 10px;
 		}
 		
 		nav ul li {
 			display: inline-block;
-			margin: 0 5px;
+			margin: 0 4px;
 		}
 		
 		nav ul li a {
 			text-decoration: none;
 			display: block;
-			border: 1px solid #ddd;
+			border: 1px solid #2a2a2a;
 			padding: 1px 10px;
 			border-radius: 5px;
-			height: 28px;
-			line-height: 28px;
-			background-color: #fafafa;
+			height: 32px;
+			line-height: 32px;
+			background-color: #111;
+			color: #0a0;
 		}
 		
 		nav ul li a:hover {
