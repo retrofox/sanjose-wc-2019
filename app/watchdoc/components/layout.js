1ef172c5eb29f2dbcb205dc6a3063b78fe5dcde0
retrofox <rdsuarez@gmail.com>
2019-09-05 15:55:54 -0600

    rebase: adding global styles

--- app/watchdoc/components/layout.js
+++ app/watchdoc/components/layout.js
@@ -10,15 +10,12 @@ import Menu from './layout-menu';
 import Footer from './layout-footer';
 import globalStyles from './global-styles';
 
-const connected = true;
-
 export default ({ children, title = 'Watchdog' }) => (
 	<div>
 		<Head>
 			<title>{title}</title>
 			<meta charSet='utf-8' />
 			<meta name='viewport' content='initial-scale=1.0, width=device-width' />
-			<style jsx global>{ globalStyles }</style>
 		</Head>
 
 		<header>
@@ -26,9 +23,12 @@ export default ({ children, title = 'Watchdog' }) => (
 		</header>
 
 		<section className="main-container">
-			{connected && children}
+			{children}
 		</section>
 
-		<footer><Footer /></footer>
+		<footer>
+			<Footer />
+		</footer>
+		<style global jsx>{ globalStyles }</style>
   </div>
 )
