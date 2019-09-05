1082b2ce8008cebf3b10d0138892af6e3d40c9b2
retrofox <rdsuarez@gmail.com>
2019-09-05 10:48:17 -0600

    refact -> create menu styles component

---
+++ app/watchdoc/components/layout-menu-styles.js
@@ -0,0 +1,60 @@
+/**
+ * External dependencies
+ */
+
+import css from 'styled-jsx/css';
+
+export default css`
+	nav {
+		width: 100%;
+		background-color: #222;
+		border-bottom: 1px solid #040;
+		position: absolute;
+		z-index: 10;
+		top: 0;
+		left: 0;
+		height: 60px;
+		padding-top: 14px;
+	}
+	
+	nav ul,
+	nav ul li {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+	
+	nav ul {
+		margin-left: 10px;
+	}
+	
+	nav ul li {
+		display: inline-block;
+		margin: 0 4px;
+	}
+	
+	nav ul li a {
+		text-decoration: none;
+		display: block;
+		border: 1px solid #2a2a2a;
+		padding: 1px 10px;
+		border-radius: 5px;
+		height: 32px;
+		line-height: 32px;
+		background-color: #111;
+		color: #0a0;
+	}
+	
+	nav ul li a:hover {
+		background-color: #444;
+		color: white;
+	}
+	
+	.react-version {
+		height: 60px;
+		line-height: 60px;
+		position: absolute;
+		right: 10px;
+		top: 0;
+	}
+`;
--- app/watchdoc/components/layout-menu.js
+++ app/watchdoc/components/layout-menu.js
@@ -7,62 +7,11 @@ import Link from "next/link";
  * Internal dependencies
  */
 import { version } from '../package';
+import menuStyles from './layout-menu-styles';
+
 
 export default () => <nav>
-	<style jsx>{`
-		nav {
-			width: 100%;
-			background-color: #222;
-			border-bottom: 1px solid #040;
-			position: absolute;
-			z-index: 10;
-			top: 0;
-			left: 0;
-			height: 60px;
-			padding-top: 14px;
-		}
- 
-		nav ul,
-		nav ul li {
-			list-style: none;
-			margin: 0;
-			padding: 0;
-		}
-		
-		nav ul {
-			margin-left: 10px;
-		}
-		
-		nav ul li {
-			display: inline-block;
-			margin: 0 4px;
-		}
-		
-		nav ul li a {
-			text-decoration: none;
-			display: block;
-			border: 1px solid #2a2a2a;
-			padding: 1px 10px;
-			border-radius: 5px;
-			height: 32px;
-			line-height: 32px;
-			background-color: #111;
-			color: #0a0;
-		}
-		
-		nav ul li a:hover {
-			background-color: #444;
-			color: white;
-		}
-		
-		.react-version {
-			height: 60px;
-			line-height: 60px;
-			position: absolute;
-			right: 10px;
-			top: 0;
-		}
-	`}</style>
+	<style jsx>{ menuStyles }</style>
 	<ul>
 		<li>
 			<Link href='/'>
