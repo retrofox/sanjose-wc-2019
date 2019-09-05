52809b8193d99a4b40addde42673ed7eb6b4878c
retrofox <rdsuarez@gmail.com>
2019-09-05 07:58:02 -0600

    add contact page

---
+++ app/watchdoc/pages/contact.js
@@ -0,0 +1,6 @@
+import Layout from '../components/layout'
+
+export default () => <Layout title="Contacto">
+	<h1>Contacto</h1>
+	<p>Enviar un ping a <a href="https://twitter.com/retrofox">@retrofox</a></p>
+</Layout>
